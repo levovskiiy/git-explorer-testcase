52947a618b77edf6a8e0437906d5ecc18ed4a115
lev cherepanov <lev.cherepanov2001@yandex.ru>
2025-09-15 22:25:12 +0500

    rename dir

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "../pages/Home/Home.vue";
-import RepoDetail from "../pages/Detial/RepoDetail.vue";
+import RepoDetail from "../pages/Detail/RepoDetail.vue";
 
 Vue.use(Router);
 
